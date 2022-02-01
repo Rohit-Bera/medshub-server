7bf37777f638780fd1c44519919b6fa80b0a943d
rohit <rohitnitaibera@gmail.com>
2022-02-01 10:22:38 +0530

    changes add

--- controllers/feedback.controller.js
+++ controllers/feedback.controller.js
@@ -15,8 +15,7 @@ const postWebFeedback = async (request, response, next) => {
     //google sheets
 
     const auth = new google.auth.GoogleAuth({
-      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS, // .json file
-      keyFilename:process.env.google-credentials.json,
+      keyFile: "google-credentials.json",
       scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
