a85d97c89eb82a4ca167de62b9778609be238c58
rohit <rohitnitaibera@gmail.com>
2022-03-10 17:12:45 +0530

    forgot password added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
-const adminAuth = require("../middlewares/adminauth")
+const adminAuth = require("../middlewares/adminauth");
 
 //controllers
-const {signUp,logIn,edit,deleteUser,getAllUsers,forgotPass,resetPass,resetPassPost} = require("../controllers/user.controller");
-//routes 
-router.post('/signUp',signUp);
-router.post('/logIn',logIn);
-router.put('/editUser/:id',auth,edit);
-router.delete('/deleteUser/:id',auth,adminAuth,deleteUser);
-router.get('/getAllUsers',auth,adminAuth,getAllUsers)
-router.post('/forgot-password',forgotPass);
-router.get('/reset-password/:id/:token',resetPass)
-
+const {
+  signUp,
+  logIn,
+  edit,
+  deleteUser,
+  getAllUsers,
+  forgotPass,
+  resetPass,
+} = require("../controllers/user.controller");
+//routes
+router.post("/signUp", signUp);
+router.post("/logIn", logIn);
+router.put("/editUser/:id", auth, edit);
+router.delete("/deleteUser/:id", auth, adminAuth, deleteUser);
+router.get("/getAllUsers", auth, adminAuth, getAllUsers);
+router.post("/forgot-password", forgotPass);
+router.put("/reset-password", resetPass);
 
 module.exports = router;
